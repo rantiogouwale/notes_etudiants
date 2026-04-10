5c7d76095ce410705279631fd7c89ee0f9e69669
OGOUWALE Moumiranti <rantiogouwale24@gmail.com>
2026-04-10 11:13:21 +0100

    Premier commit du fichier notes.py

--- notes.py
+++ notes.py
@@ -4,3 +4,11 @@ def ajouter_etudiants(nom,note):
 etudiants.append({"nom":nom,"note":note})
 print(f"Etudiant {nom} ajouté avec la note}."}
 
+def calculer_moyenne():
+if not etudiants:
+print("Aucun etudiant enregistre.")
+return 0
+total = sum(e['note'] for e in etudiants)
+moyenne= total / len(etudiants)
+print(f"Moyenne de la classe : {moyenne:.2f}")
+return moyenne
