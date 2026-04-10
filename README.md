63bf51d12cde11dfa30cef77b0feafa4cb050881
OGOUWALE Moumiranti <rantiogouwale24@gmail.com>
2026-04-10 11:05:35 +0100

    Premier commit des fichiers

---
+++ README.md
@@ -0,0 +1,3 @@
+#Notes Etudiants
+Porjet de calcul et gestion des notes d'etudiants en Python
+
---
+++ notes.py
@@ -0,0 +1,6 @@
+etudiants = []
+
+def ajouter_etudiants(nom,note):
+etudiants.append({"nom":nom,"note":note})
+print(f"Etudiant {nom} ajouté avec la note}."}
+
